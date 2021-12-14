4f3dc4a1774e82e4a64dd13737bc828a0c7de55e
wy <hustwatson@gmail.com>
2021-12-14 15:18:28 +0800

    add fast test option

--- test/role/test.go
+++ test/role/test.go
@@ -6,9 +6,15 @@ import (
 	"log"
 	"math/big"
 	callconts "memoContract/callcontracts"
+	iface "memoContract/interfaces"
 	"memoContract/test"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+)
+
+const (
+	Fast bool = true
 )
 
 var (
@@ -28,28 +34,66 @@ func main() {
 	flag.Parse()
 	ethEndPoint = *eth
 	qethEndPoint = *qeth
+	_ = qethEndPoint
 	callconts.EndPoint = ethEndPoint
 
-	roleAddr := common.HexToAddress("0x0aE0e81C338E9128Aa630Ece410ee73F0CEEc88c")
-	// rtokenAddr := common.HexToAddress("0x1c40028BB314D4Ecd3Ead67104fb5D790Fe52F9F")
-	// rolefsAddr := common.HexToAddress("0x5c3C6b6aEfaA577548B07871688e109787d7F225")
-	// pledgePoolAddr := common.HexToAddress("0x4FD381B8b7f96B8BE2B6044c795A380EE47CF640")
-	// issuanceAddr := common.HexToAddress("0x9b2D613F811895fb67BF743A745caD256F6Ce2e0")
-	var gIndex uint64 = 1
+	// vars for test
+	var (
+		roleAddr       common.Address
+		rtokenAddr     common.Address
+		rolefsAddr     common.Address
+		pledgePoolAddr common.Address
+		issuanceAddr   common.Address
+		gIndex         uint64
+	)
+
+	// fast test use existing contract addresses
+	if Fast {
+		roleAddr = common.HexToAddress("0x0aE0e81C338E9128Aa630Ece410ee73F0CEEc88c")
+		rtokenAddr = common.HexToAddress("0x1c40028BB314D4Ecd3Ead67104fb5D790Fe52F9F")
+		rolefsAddr = common.HexToAddress("0x5c3C6b6aEfaA577548B07871688e109787d7F225")
+		pledgePoolAddr = common.HexToAddress("0x4FD381B8b7f96B8BE2B6044c795A380EE47CF640")
+		issuanceAddr = common.HexToAddress("0x9b2D613F811895fb67BF743A745caD256F6Ce2e0")
+		gIndex = 1
+	} else {
+		_ = roleAddr
+		_ = rtokenAddr
+		_ = rolefsAddr
+		_ = pledgePoolAddr
+		_ = issuanceAddr
+		_ = gIndex
+	}
 
 	// 用于测试的一些参数
 	adminAddr := common.HexToAddress(test.AdminAddr)
-	// rechargeMoney := big.NewInt(1e8)
-	// start := uint64(time.Now().Unix()) // 当前时间的时间戳
-	// end := start + 10
-	// size := uint64(10)
-	// sprice := big.NewInt(100)
+
 	// 部署Role的参数
+	var (
+		rechargeMoney *big.Int
+		start         uint64
+		end           uint64
+		size          uint64
+		sprice        *big.Int
+	)
+	// fast test don't use them
+	if !Fast {
+		rechargeMoney = big.NewInt(1e8)
+		start = uint64(time.Now().Unix()) // 当前时间的时间戳
+		end = start + 10
+		size = uint64(10)
+		sprice = big.NewInt(100)
+	}
+	_ = rechargeMoney
+	_ = start
+	_ = end
+	_ = size
+	_ = sprice
 
 	pledgeK := big.NewInt(1e18)
-	// pledgeP := big.NewInt(1e18)
+	pledgeP := big.NewInt(1e18)
+	_ = pledgeP
 	var addrs []common.Address = []common.Address{common.HexToAddress(test.Acc1), common.HexToAddress(test.Acc2), common.HexToAddress(test.Acc3), common.HexToAddress(test.Acc4), common.HexToAddress(test.Acc5)}
-	//var sks []string = []string{test.Sk1, test.Sk2, test.Sk3, test.Sk4, test.Sk5}
+	var sks []string = []string{test.Sk1, test.Sk2, test.Sk3, test.Sk4, test.Sk5}
 
 	// 查看余额，支付交易Gas费，余额不足时，需充值（暂时手动）
 	bal := callconts.QueryEthBalance(test.AdminAddr, ethEndPoint)
@@ -66,7 +110,7 @@ func main() {
 		GasLimit: callconts.DefaultGasLimit,
 	}
 
-	fmt.Println(">>>> Prepairing balances")
+	fmt.Println(">>>> checking balance of admin, 5 eth at least")
 	// 查看测试账户的ERC20代币余额，不足时自动充值
 	erc20 := callconts.NewERC20(adminAddr, test.AdminSk, txopts)
 	// 确保admin账户的ERC20代币余额充足（至少5 eth）
@@ -87,6 +131,8 @@ func main() {
 		}
 		fmt.Println("after mint, admin balance in primaryToken is ", bal)
 	}
+
+	fmt.Println(">>>> checking balance of accounts, 1 eth each")
 	// 确保每个测试账户的ERC20代币余额充足（不少于Pledge值，默认1 eth）
 	for i, addr := range addrs {
 		bal, err = erc20.BalanceOf(test.PrimaryToken, addr)
@@ -120,10 +166,14 @@ func main() {
 	// 调用<Role合约>中的CreateGroup函数,自动部署<FileSys合约>
 	// 顺序：Role、RoleFS、PledgePool、CreateGroup(FileSys)
 
-	// deploy Role
 	r := callconts.NewR(adminAddr, test.AdminSk, txopts)
-	/*
-		// fmt.Println(">>>> begin deploy Role")
+
+	// deploy Role
+	if Fast {
+		fmt.Println("@@ fast test skip deploy Role, use existing address")
+	} else {
+
+		fmt.Println(">>>> begin deploy Role")
 
 		// 部署Role合约的时候指定使用的ERC20 Token,以及最小的Pledge值
 		roleAddr, _, err := r.DeployRole(test.Foundation, test.PrimaryToken, pledgeK, pledgeP)
@@ -148,15 +198,22 @@ func main() {
 		}
 		fmt.Println("----> The RoleFS contract address: ", rolefsAddr.Hex())
 
-	*/
+	}
 
-	// fmt.Println(">>>> begin register accountss")
 	// register accounts
 	// acc1: User
 	// acc2, acc3, acc4: Keeper
 	// acc5: Provider
-	/*
-		rIndexes := make([]uint64, 5)
+
+	var rIndexes []uint64
+
+	// const slice for fast test
+	if Fast {
+		fmt.Println("@@ fast test skip register accounts, use default role indexes")
+		rIndexes = []uint64{1, 2, 3, 4, 5}
+	} else {
+		fmt.Println(">>>> begin register accountss to get role indexes")
+		rIndexes = make([]uint64, 5)
 		for i, addr := range addrs {
 			r = callconts.NewR(addr, sks[i], txopts)
 			err = r.Register(roleAddr, addr, nil)
@@ -168,73 +225,91 @@ func main() {
 				log.Fatal(err)
 			}
 		}
-		fmt.Println("Role indexes is ", rIndexes)
-	*/
-	rIndexes := []uint64{1, 2, 3, 4, 5}
 
-	// fmt.Println(">>>> begin deploy PledgePool")
+	}
 
-	/*
+	fmt.Println("Role indexes is ", rIndexes)
+
+	var p iface.PledgePoolInfo
+	// register accounts
+	if Fast {
+		fmt.Println("@@ fast test skip register accounts, use default role indexes")
+	} else {
+		fmt.Println(">>>> begin deploy PledgePool")
 		p := callconts.NewPledgePool(adminAddr, test.AdminSk, txopts)
 		pledgePoolAddr, _, err := p.DeployPledgePool(test.PrimaryToken, rtokenAddr, roleAddr)
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println("----> The PledgePool contract address: ", pledgePoolAddr.Hex())
-		//pledgePoolAddr := common.HexToAddress("0xfB279b29E437cEE73ac2F2423a3Cfb22060Eb9d7")
-	*/
-
-	// get min Pledge for keeper and provider
-	pledgek, err := r.PledgeK(roleAddr) // 申请Keeper最少需质押的金额
-	if err != nil {
-		log.Fatal(err)
 	}
-	fmt.Println("min pledge for applying keeper: ", pledgek)
-	pledgep, err := r.PledgeP(roleAddr) // 申请Provider最少需质押的金额
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("min pledge for applying provider: ", pledgep)
 
-	/*
+	// get pledge
+	var pledgek *big.Int
+	var pledgep *big.Int
+	if Fast {
+		fmt.Println("@@ fast test skip get Pledge, use default: 1 eth")
+	} else {
+		fmt.Println(">>>> Getting min pledge from Role")
+		// get min Pledge for keeper and provider
+		pledgek, err := r.PledgeK(roleAddr) // 申请Keeper最少需质押的金额
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("min pledge for applying keeper: ", pledgek)
+		pledgep, err := r.PledgeP(roleAddr) // 申请Provider最少需质押的金额
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("min pledge for applying provider: ", pledgep)
+	}
+
+	// pledge
+	if Fast {
+		fmt.Println("@@ fast test skip Pledge, use existing")
+	} else {
 		fmt.Println(">>>> begin pledge")
-		// Keepers pledge minPledgeK for register
+		fmt.Println("Keepers pledge minPledgeK for register")
 		for i, rindex := range rIndexes[1:4] {
 			err = toPledge(addrs[i+1], roleAddr, pledgePoolAddr, sks[i+1], rindex, pledgek, txopts)
 			if err != nil {
 				log.Fatal(err)
 			}
 		}
-		// Provider pledge minPledgeP for register
+		fmt.Println("Provider pledge minPledgeP for register")
 		err = toPledge(addrs[4], roleAddr, pledgePoolAddr, sks[4], rIndexes[4], pledgep, txopts)
 		if err != nil {
 			log.Fatal(err)
 		}
-	*/
+	}
 
-	// fmt.Println(">>>> begin deploy Issuance")
-	/*
+	// deploy Issuance
+	if Fast {
+		fmt.Println("@@ fast test skip deploy Issuance")
+	} else {
+		fmt.Println(">>>> begin deploy Issuance")
+
 		issu := callconts.NewIssu(adminAddr, test.AdminSk, txopts)
 
-			issuanceAddr, _, err := issu.DeployIssuance(rolefsAddr)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println("----> The Issuance contract address is: ", issuanceAddr.Hex()) // 0xB15FEDB8017845331b460786fb5129C1Da06f6B1
-	*/
+		issuanceAddr, _, err := issu.DeployIssuance(rolefsAddr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("----> The Issuance contract address is: ", issuanceAddr.Hex()) // 0xB15FEDB8017845331b460786fb5129C1Da06f6B1
 
-	/*
 		// 给Role合约指定所有相关合约地址
 		r = callconts.NewR(adminAddr, test.AdminSk, txopts)
 		err = r.SetPI(roleAddr, pledgePoolAddr, issuanceAddr, rolefsAddr)
 		if err != nil {
 			log.Fatal(err)
 		}
-	*/
+	}
 
-	/*
+	// register keepers and provider
+	if Fast {
+		fmt.Println("@@ fast test skip deploy keeper and provider")
+	} else {
 		fmt.Println(">>>> begin register keepers")
-		// Register Keepers
 		callconts.PledgePoolAddr = pledgePoolAddr
 		for i, rindex := range rIndexes[1:4] {
 			r := callconts.NewR(addrs[i+1], sks[i+1], txopts)
@@ -261,7 +336,6 @@ func main() {
 		}
 
 		fmt.Println(">>>> begin register provider")
-		// Register Provider
 		r = callconts.NewR(addrs[4], sks[4], txopts)
 		// isActive, isBanned, roleType, index, gIndex, extra
 		isActive, _, roleType, index, gIndex, _, err := r.GetRoleInfo(roleAddr, addrs[4])
@@ -276,14 +350,13 @@ func main() {
 				log.Fatal(err)
 			}
 		}
-	*/
 
-	fmt.Println("INFOS:")
-	fmt.Println("rIndexes: ", rIndexes)
-	fmt.Println("addrs: ", addrs)
-	fmt.Println("gIndex: ", gIndex)
+	}
 
-	/*
+	// create group
+	if Fast {
+		fmt.Println("@@ fast test skip create group")
+	} else {
 		fmt.Println(">>>> begin create group")
 		// 需要admin先调用CreateGroup,同时将部署FileSys合约
 		r = callconts.NewR(adminAddr, test.AdminSk, txopts)
@@ -291,7 +364,12 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-	*/
+	}
+
+	fmt.Println("INFOS:")
+	fmt.Println("rIndexes: ", rIndexes)
+	fmt.Println("addrs: ", addrs)
+	fmt.Println("gIndex: ", gIndex)
 
 	// 获取group信息
 	fmt.Println(">>>> getting group info")
@@ -314,17 +392,14 @@ func main() {
 	fmt.Println("============ 2. test GetAddrGindex ============")
 
 	// get acc address and gIndex by rIndex
-	acc, gi, err := r.GetAddrGindex(roleAddr, 0)
+	acc, gi, err := r.GetAddrGindex(roleAddr, 3)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("acc: ", acc, " gi: ", gi)
-	if acc != addrs[1] {
+	if acc != addrs[0] {
 		log.Fatal("test GetAddrGindex failed: acc address error")
-	}
-	if gi != gIndex {
-		log.Fatal("test GetAddrGindex failed: gIndex error")
 	}
 
 	fmt.Println("============test success!============")
