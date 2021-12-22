6ca97161c8ef41108e5718f08aa3755810790b5d
zl <1019764004@qq.com>
2021-12-22 19:30:53 +0800

    add money-cliCommand,add array index check

--- cmd/admin.go
+++ cmd/admin.go
@@ -15,6 +15,14 @@ import (
 var AdminCmd = &cli.Command{
 	Name:  "admin",
 	Usage: "Admin deploy and call contracts",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:    "erc20Addr",
+			Aliases: []string{"e"},
+			Value:   "0xa96303D074eF892F39BCF5E19CD25Eeff7A73BAA", //默认值为common.go中的erc20合约地址
+			Usage:   "the ERC20 contract address",
+		},
+	},
 	Subcommands: []*cli.Command{
 		deployERC20Cmd,
 		mintCmd,
@@ -23,7 +31,7 @@ var AdminCmd = &cli.Command{
 
 var deployERC20Cmd = &cli.Command{
 	Name:  "deploy-erc20",
-	Usage: "admin deploy ERC20 contract",
+	Usage: "admin deploy ERC20 contract. Args0:name, Args1:symbol",
 	Action: func(cctx *cli.Context) error {
 		name := cctx.Args().Get(0)
 		symbol := cctx.Args().Get(1)
@@ -47,20 +55,17 @@ var deployERC20Cmd = &cli.Command{
 
 var mintCmd = &cli.Command{
 	Name:  "mint",
-	Usage: "admin mint ERC20 token",
-	Flags: []cli.Flag{
-		&cli.StringFlag{
-			Name:  "erc20Addr",
-			Usage: "indicate the ERC20 contract address",
-			Value: "0xa96303D074eF892F39BCF5E19CD25Eeff7A73BAA",
-		},
-	},
+	Usage: "admin mint ERC20 token. Args0:target, Args1:amount",
 	Action: func(cctx *cli.Context) error {
 		target := cctx.Args().Get(0)
 		amount := cctx.Args().Get(1)
-		fmt.Println("amount", amount, "target:", target)
+		fmt.Println("amount:", amount, ", target:", target)
 
 		// TODO:check target
+		if len(target) != 42 {
+			fmt.Println("target should be with prefix 0x")
+			return nil
+		}
 
 		mintValue := big.NewInt(0)
 		mintValue, ok := mintValue.SetString(amount, 10)
@@ -69,6 +74,7 @@ var mintCmd = &cli.Command{
 		}
 		if mintValue.Cmp(big.NewInt(0)) <= 0 {
 			fmt.Println("mintValue should be more than 0")
+			return nil
 		}
 
 		txopts := &callconts.TxOpts{
@@ -76,7 +82,10 @@ var mintCmd = &cli.Command{
 			GasPrice: big.NewInt(callconts.DefaultGasPrice),
 			GasLimit: callconts.DefaultGasLimit,
 		}
+
 		erc20Addr := common.HexToAddress(cctx.String("erc20Addr"))
+		fmt.Println("erc20Addr:", erc20Addr.Hex())
+
 		e := callconts.NewERC20(erc20Addr, callconts.AdminAddr, callconts.AdminSk, txopts)
 		err := e.MintToken(common.HexToAddress(target), mintValue)
 		if err != nil {
